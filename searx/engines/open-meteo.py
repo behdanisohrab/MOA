73e7abe8a4b97b63a1c472bd7b50fcdda28c2726
mehrdad <qxc9966@protonmail.com>
2023-08-22 12:24:20 +0330

    added new engines semi-finished

---
+++ searx/engines/open-meteo.py
@@ -0,0 +1,5 @@
+# SPDX-License-Identifier: AGPL-3.0-or-later
+# lint: pylint
+"""wttr.in (weather forecast service)"""
+
+from json import loads
---
+++ searx/engines/openlibrary.py
@@ -0,0 +1,1 @@
+https://openlibrary.org/search.json?q=
