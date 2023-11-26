92ba5a01e657a227016f1f15e1a6fc5a2952e11c
Gnkalk <gnkalk@yahoo.com>
2023-11-26 22:33:35 +0330

    Improve infobox & fix links on top

--- searx/static/themes/simple/src/js/reactivity.js
+++ searx/static/themes/simple/src/js/reactivity.js
@@ -1,36 +1,51 @@
-function $(selector) {
-    return document.querySelector(selector);
+function $ (selector) {
+  return document.querySelector(selector);
 }
 
-function toggleBtn(thisEl, activeClass, targetSelector, targetClass) {
-    if (activeClass) {
-        thisEl.classList.toggle(activeClass);
-    }
-    $(targetSelector).classList.toggle(targetClass);
+function $$ (selector) {
+  return document.querySelectorAll(selector);
 }
 
-function addBtn(thisEl, activeClass, targetSelector, targetClass) {
-    if (activeClass) {
-        thisEl.classList.add(activeClass);
-    }
-    $(targetSelector).classList.add(targetClass);
+function toggleBtn (thisEl, activeClass, targetSelector, targetClass) {
+  if (activeClass) {
+    thisEl.classList.toggle(activeClass);
+  }
+  $(targetSelector).classList.toggle(targetClass);
 }
 
-function removeBtn(thisEl, activeClass, targetSelector, targetClass) {
-    if (activeClass) {
-        thisEl.classList.remove(activeClass);
-    }
-    $(targetSelector).classList.remove(targetClass);
+function addBtn (thisEl, activeClass, targetSelector, targetClass) {
+  if (activeClass) {
+    thisEl.classList.add(activeClass);
+  }
+  $(targetSelector).classList.add(targetClass);
+}
+
+function removeBtn (thisEl, activeClass, targetSelector, targetClass) {
+  if (activeClass) {
+    thisEl.classList.remove(activeClass);
+  }
+  $(targetSelector).classList.remove(targetClass);
 }
 
 function imagesPreview() {
-    $(".preview-images").classList.add("collapse-open");
-    $(".media-more-btn").addEventListener("click", () => {
-        $("#checkbox_images").checked = true;
-    });
-    $(".media-more-btn").onclick = null;
+  $(".preview-images").classList.add("collapse-open");
+  $(".media-more-btn").addEventListener("click", () => {
+    $("#checkbox_images").checked = true;
+  });
+  $(".media-more-btn").onclick = null;
 }
 
 function videosPreview() {
-    $("#checkbox_videos").checked = true;
+  $("#checkbox_videos").checked = true;
+}
+
+function infoboxMore() {
+  $$(".infobox").forEach((infobox) => {
+    const moreBtn = infobox.querySelector(".infobox-more-btn")
+    if (moreBtn) {
+      moreBtn.addEventListener("click", () => {
+        infobox.classList.toggle("collapse-open");
+      })
+    }
+  })
 }
