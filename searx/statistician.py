7c75232cecad890df1d0cf37e636971be9f1668e
mehrdad <qxc9966@protonmail.com>
2024-03-14 12:40:53 +0330

    Modifying the statistics structure to avoid errors in Docker builds

--- searx/statistician.py
+++ searx/statistician.py
@@ -1,8 +1,17 @@
 import datetime
+import os
 
 def log_statistics(grouping, number=""):
-    file_name="searx/statistics.log"
-    with open(file_name, 'a') as log_file:
-        timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
-        log_entry = f"{timestamp} | Number of results: {number}| Grouping: {grouping}\n"
-        log_file.write(log_entry)
+    file_name = "searx/statistics.log"
+
+    try:
+        with open(file_name, 'a') as log_file:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+            log_entry = f"{timestamp} | Number of results: {number} | Grouping: {grouping}\n"
+            log_file.write(log_entry)
+    except FileNotFoundError:
+        os.makedirs(os.path.dirname(file_name), exist_ok=True)
+        with open(file_name, 'w') as log_file:
+            timestamp = datetime.datetime.now().strftime("%Y-%m-%d")
+            log_entry = f"{timestamp} | Log file created | Number of results: {number} | Grouping: {grouping}\n"
+            log_file.write(log_entry)
